dc2157f0a0b697d67241396a7330b84d40695a21
Dmytro Haidenko <d3haid@gmail.com>
2017-09-16 21:18:27 +0300

    Commit2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+//
 public class Main {
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Yo");
+        System.out.println("Please enter you Name and press 'Enter' button");
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        // write your code here
+        System.out.println(name + ", is this your name?");
+        Scanner no = new Scanner(System.in);
+        Boolean Answer = no.nextBoolean();
+        System.out.println("Answer = " + Answer);
     }
 }
