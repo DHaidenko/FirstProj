a6c13cd99ab1504ce11317f728c73fdf6ee33858
dmytro.haidenko <d3haid@gmail.com>
2017-10-05 12:33:22 +0300

    Igor told me always write comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,36 @@
 package com.company;
 
 import java.util.Scanner;
+
 //
 public class Main {
     public static void main(String[] args) {
+        /*1. Приветствовать любого пользователя при вводе его имени через командную строку.
+        nameCheck();*/
+        //2. Отобразить в окне консоли аргументы командной строки в обратном порядке.
+        TextPursing();
+        Scanner numx = new Scanner(System.in);
+        int[] nums = new int[5];
+        for (int i = 0; i < numx.nextInt(); i++) {
+            switch (nums[i] = (int) Math.random()) {
+            }
+            System.out.println(nums[i]);
+        }
+    }
+
+    private static void nameCheck() {
         System.out.println("Please enter you Name and press 'Enter' button");
         Scanner in = new Scanner(System.in);
-        String name = in.nextLine();
-        // write your code here
-        System.out.println(name + ", is this your name?");
-        Scanner no = new Scanner(System.in);
-        Boolean Answer = no.nextBoolean();
-        System.out.println("Answer = " + Answer);
+        System.out.println("Great name, " + in.next());
     }
+
+    private static void TextPursing() {
+        Scanner x = new Scanner(System.in);
+        String text = x.nextLine();
+        String[] word = text.split(" ");
+        for (int i = word.length - 1; i >= 0; i--) {
+            System.out.println("args[" + i + "] : " + word[i]);
+        }
+    }
+
 }
